1c4752ad4ae4254e6524f7c976456180010bdd46
Minh Quang <letuyettrinh10c12@gmail.com>
2020-05-25 22:04:05 +0700

    imporve 3 file cat index and mouse

--- cat.js
+++ cat.js
@@ -1,9 +1,15 @@
+var mouse = require('./mice')
 function cat() {
     this.stomach = [];
 }
 
-cat.prototype.eat = function(){
-    this.stomach.push(mice);
+cat.prototype.eat = function (animal) {
+    if (animal instanceof mouse) {
+        this.stomach.push(animal);
+    }
+    else {
+        throw new Error('Cat can only eat mouse !');
+    }
 }
 
 module.exports = cat;
